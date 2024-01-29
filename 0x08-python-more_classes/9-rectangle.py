0ffd2f80b1c3c61e0164e36395f8bc293195277b
MondeAnna <monde.anna@gmail.com>
2024-01-29 16:16:19 +0200

    OOP II (Task 09): Square Factory
        - Course Instructions:
            - Write a class Rectangle that defines a
              rectangle
    
        - Instantiation:
            - `def __init__(self, width=0, height=0):`
    
        - Private Instance Attributes:
            - `width`
            - `height`
    
        - Properties
            - `def width(self):` -> retrieves `width`
            - `def width(self, value):` -> sets `width`
            - `def height(self):` -> retrieves `height`
            - `def height(self, value):` -> sets `height`
    
       - Public Instance Method:
            - `def area(self):` -> returns the area
            - `def perimeter(self):` -> returns the perimeter
    
        - Static Method:
            - `def bigger_or_equal(rect_1, rect_2):`
              -> returns the biggest rectangle based on
                 the area, `rect_1` if the same equal
    
        - Spec:
            - `width`/`height` must be an integer, otherwise
              raise a `TypeError` exception with the message
              `width must be an integer` or `height must be
              an integer`
            - if `width`/`height` is less than `0`, raise a
              `ValueError` exception with the message `width
              must be >= 0` or `height must be >= 0`
            - if `width` or `height` is equal to `0`,
              `perimeter` is equal to `0`
            - `print()` and `str()` should print the rectangle
              with the character `#`:
            - if `width` or `height` is equal to `0`, return
              an empty string
            - `rect_1`/`rect_2` must be an instance of `Rectangle`
               otherwise raise a `TypeError` exception with the
               message `rect_1 must be an instance of Rectangle`
               or `rect_2 must be an instance of Rectangle`
            - You are not allowed to import any module
    
        - Example:
        ```
        guillaume@ubuntu:~/0x08$ cat 9-main.py
        #!/usr/bin/python3
        Rectangle = __import__('9-rectangle').Rectangle
    
        my_square = Rectangle.square(5)
        print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
        print(my_square)
    
        guillaume@ubuntu:~/0x08$ ./9-main.py
        Area: 25 - Perimeter: 20
        #####
        #####
        #####
        #####
        #####
        Bye rectangle...
        guillaume@ubuntu:~/0x08$
        ```

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+
+"""
+Rectangle Class
+"""
+
+
+class Rectangle:
+    """
+    Rectangle Class
+    """
+
+    number_of_instances = 0
+    __print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def print_symbol(self):
+        return self.__print_symbol
+
+    @print_symbol.setter
+    def print_symbol(self, value):
+        self.__print_symbol = value
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        self.__height = value
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__width = value
+
+    def area(self):
+        """get area"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """get perimeter"""
+        if not self.height or not self.width:
+            return 0
+        return self.height * 2 + self.width * 2
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """compare rectangles"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+    def __str__(self):
+        """get graphical representation"""
+        str_ = ""
+        symbol = str(self.print_symbol)
+        if not self.height or not self.width:
+            return str_
+        for _ in range(self.height):
+            str_ += symbol * self.width + "\n"
+        return str_[:-1]
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
