dc77a65f406e07aaba0d827f7b3a30a515aa7c04
MondeAnna <monde.anna@gmail.com>
2024-02-05 12:06:07 +0200

    Inheritance (Task 01): Inherit list Class
        - Course Instructions:
            - Write a class MyList that inherits from `list`
    
        - Public instance method:
            - `def print_sorted(self):`
    
        - Specs:
            - `print_sorted` prints the list in ascending
              order
            - You can assume that all the elements of the
              list will be of type `int`
            - You are not allowed to import any module
    
        - Example:
        ```
        guillaume@ubuntu:~/0x0A$ cat 1-main.py
        #!/usr/bin/python3
        MyList = __import__('1-my_list').MyList
    
        my_list = MyList()
        my_list.append(1)
        my_list.append(4)
        my_list.append(2)
        my_list.append(3)
        my_list.append(5)
        print(my_list)
        my_list.print_sorted()
        print(my_list)
    
        guillaume@ubuntu:~/0x0A$ ./1-main.py
        [1, 4, 2, 3, 5]
        [1, 2, 3, 4, 5]
        [1, 4, 2, 3, 5]
        guillaume@ubuntu:~/0x0A$
        ```

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+MyList, a class that inherits from `list`
+"""
+
+
+class MyList(list):
+    """
+    MyList, a class that inherits from `list`
+    """
+
+    def print_sorted(self):
+        """
+        prints ascendend sort of list
+        """
+        print(sorted(self))
