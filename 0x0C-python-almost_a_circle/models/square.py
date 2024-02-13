11cf438acd25e1f0e39003e4cdab9eb88c850553
MondeAnna <monde.anna@gmail.com>
2024-02-13 17:45:34 +0200

    Circle (Task 11): Square -> Add setter/getter for `size`
        - Course Instructions:
            - Update the class `Square` by adding the
              public getter and setter `size`
    
        - Public Method:
            - `size`
    
        - Specs:
            - The setter should assign (in this order)
              the `width` and the `height`
            - with the same value
            - The setter should have the same value
              validation as the `Rectangle` for `width`
              and `height`
            - No need to change the exception error
              message (It should be the one from `width`)
    
        - Example:
        ```
        guillaume@ubuntu:~/$ cat 10-main.py
        #!/usr/bin/python3
        """ 10-main """
        from models.square import Square
    
        if __name__ == "__main__":
    
            s1 = Square(5)
            print(s1)
            print(s1.size)
            s1.size = 10
            print(s1)
    
            try:
                s1.size = "9"
            except Exception as e:
                print("[{}] {}".format(e.__class__.__name__, e))
    
        guillaume@ubuntu:~/$ ./10-main.py
        [Square] (1) 0/0 - 5
        5
        [Square] (1) 0/0 - 10
        [TypeError] width must be an integer
        guillaume@ubuntu:~/$
        ```
    
    # Conflicts:
    #	0x0C-python-almost_a_circle/models/square.py
    
    # Conflicts:
    #	0x0C-python-almost_a_circle/models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,26 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
+    @property
+    def size(self):
+        """ provide the size of a side of a square """
+
+        return self._Rectangle__width
+
+    @size.setter
+    def size(self, value):
+        """
+        set size of square
+
+        Parameter
+        ---------
+        value : int
+            value to be assigned as size
+        """
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ return implementation of repr for str() """
 
